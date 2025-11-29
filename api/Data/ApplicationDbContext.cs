737dbe059d4487a49732c375a72731ad75657eb7
Dan Ledezma <daledezma2020@gmail.com>
2025-11-29 14:54:35 -0500

    chore: setting up db and gitignore

---
+++ api/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Add your DbSets here later
+    // public DbSet<Workout> Workouts { get; set; }
+}
